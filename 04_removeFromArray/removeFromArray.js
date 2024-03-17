6536cc89a6fc39de7aa38498e30f9876ec385ea3
Mikaela <19698567+mikaelabelle@users.noreply.github.com>
2024-03-17 17:07:03 +1000

    first lot done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray, ...args) {
+    const result = inputArray.filter((input) => !args.includes(input))
+    return result
 };
+
+// console.log(removeFromArray([1, 2, 3, 4], 4, 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
+const sumAll = function (x, y) {
+    if (x < 0 || y < 0 || typeof x != "number" || typeof y != "number") {
+        return "ERROR"
+    }
+    let total = 0
+    if (x < y) {
+        for (let i = x; i <= y; i++) {
+            total += i
+        }
+    }
+    else {
+        for (let i = y; i <= x; i++) {
+            total += i
+        }
+    }
+    return total
+}
 
 // Do not edit below this line
 module.exports = sumAll;
