719e33773eaed73585fe1fd6b7230dd8a4068676
Mikaela <19698567+mikaelabelle@users.noreply.github.com>
2024-03-17 16:04:51 +1000

    first few exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (inputString) {
+    let letters = inputString.split("");
+    let reverseArray = []
+    for (let i = 0; i < letters.length; i++) {
+        reverseArray.unshift(letters[i])
+    }
+    let reverseString = reverseArray.join("")
+    return reverseString;
 };
 
 // Do not edit below this line
