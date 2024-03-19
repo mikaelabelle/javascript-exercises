9aac497d2534e3679631d347a2d949afb2f1c2a1
Mikaela Bell <19698567+mikaelabelle@users.noreply.github.com>
2024-03-19 14:34:49 +1000

    calc exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, num) => total + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((total, num) => total * num, 1)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let array = []
+  for (let i = 1; i <= num; i++) {
+    array.push(i)
+  }
+  return array.reduce((total, indiv) => total * indiv, 1)
 };
 
 // Do not edit below this line
