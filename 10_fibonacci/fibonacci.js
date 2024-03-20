9c5401e5519392f228f79cc15433e7c4ad38d870
Mikaela <19698567+mikaelabelle@users.noreply.github.com>
2024-03-20 16:10:27 +1000

    rest of exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    input = parseInt(num) - 1
 
+    if (num < 0) {
+        return "OOPS"
+    }
+    else if (num == 0) {
+        return 0
+    }
+
+    let fibo = [1, 1]
+    for (let i = 1; i <= input; i++) {
+        fibo.push(fibo[i] + fibo[i - 1])
+    }
+
+    return fibo[input]
 };
+
+console.log(fibonacci("1"))
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    let sorted = array.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear()
+        }
+        else if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear()
+        }
 
+        let aAge = a.yearOfDeath - a.yearOfBirth
+        let bAge = b.yearOfDeath - b.yearOfBirth
+
+        return aAge > bAge ? -1 : 1
+
+    })
+
+    return sorted[0]
 };
 
 // Do not edit below this line
