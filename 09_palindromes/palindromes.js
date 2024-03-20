460b9ebe843b3d35c1c63361f7042255f501cb2f
Mikaela <19698567+mikaelabelle@users.noreply.github.com>
2024-03-20 15:28:48 +1000

    palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let inputChar = input.replace(/[.,\/#!$%\^&\*\s;:{}=\-_`~()]/g, "").toLowerCase()
+    let split = [...inputChar]
+    let reversed = []
+    for (let i = 0; i < split.length; i++) {
+        reversed.unshift(split[i])
+    }
+    let counter = 0
+    for (let i = 0; i < split.length; i++) {
+        reversed[i] != split[i] ? counter++ : null
+    }
+    return counter === 0 ? true : false
 
 };
 
